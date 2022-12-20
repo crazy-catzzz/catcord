2af1e2ae245f1baa6b6c4b954563ffdb1c278ffb
Davide Guerrazzi <g.davide2005@gmail.com>
2022-12-20 18:45:33 +0100

    Rebase

---
+++ src/main/java/com/crazycatzzz/App.java
@@ -0,0 +1,36 @@
+package com.crazycatzzz;
+
+import com.crazycatzzz.client.DiscordClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public final class App {
+    public static void main(String[] args) {
+        String token = "YOUR TOKEN GOES HERE";  // NOTE: This is a user token, NOT a bot token!
+        DiscordClient client = new DiscordClient(token);
+
+        client.getMe(); // GET USER INFO
+
+        // SEND MSG/SHOW LAST MSGS
+        String channel = "CHANNEL ID GOES HERE";
+        String content = "Hello World!";
+        //printChannelMsgs(channel, client);    // Uncomment if you want to print the last 10 msgs or so
+        client.sendMessage(channel, content, false, "123");
+    }
+
+    // Prints the last 10 msgs sent in a channel
+    private static void printChannelMsgs(String id, DiscordClient client) {
+        JSONObject msgs = client.getMessages(id, 10);
+
+        JSONArray msgArray = msgs.getJSONArray("messages");
+
+        // This for goes backwards since the messages in msgArray are in reverse order, from the last to the first
+        for (int i = msgArray.length() - 1; i >= 0; i--) {
+            String author = msgArray.getJSONObject(i).getJSONObject("author").getString("username");
+            String content = msgArray.getJSONObject(i).getString("content");
+
+            System.out.println(author + ": " + content);
+        }
+    }
+}
---
+++ src/main/java/com/crazycatzzz/client/DiscordClient.java
@@ -0,0 +1,179 @@
+package com.crazycatzzz.client;
+
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import org.json.JSONObject;
+
+// Custom Discord client
+public class DiscordClient {
+    private DiscordWebSocket ws; // WIP WebSocket for receiving messages etc...
+
+    //private URL loginUrl;
+    private URL meUrl;
+    //private String settingsUrl = "https://discordapp.com/api/v6/users/@me/settings";
+    //private String guildsUrl = "https://discordapp.com/api/v6/users/@me/guilds";
+    private URL gatewayUrl;
+    //private String logoutUrl = "https://discordapp.com/api/v6/auth/logout";
+    //private String trackUrl = "https://discordapp.com/api/v6/track";
+    //private String membersUrl = "https://discordapp.com/api/v6/guilds/{}/members";
+
+    private String token; // user token
+
+    private String webSocketGatewayQueryParams = "/?encoding=json&v=6"; // other websocket stuff
+
+    private JSONObject me; // user info
+
+    // HTTP Client for requests
+    HttpClient client;
+
+    public DiscordClient(String token) {
+        this.token = token;
+
+        try {
+            client = HttpClient.newHttpClient();
+
+            //loginUrl = new URL("https", "discordapp.com", "/api/v6/auth/login");
+            meUrl = new URL("https", "discordapp.com", "/api/v6/users/@me");
+            gatewayUrl = new URL("https", "discordapp.com", "/api/v6/gateway");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /*
+     * The method login(email, password) does not pass the captcha
+     * 
+    @Deprecated
+    public void login(String email, String password) {
+        HttpRequest loginReq;
+        
+        // JSON Body
+        BodyPublisher bp = BodyPublishers.ofString(
+            new String(
+                "{" +
+                    "\"email\":\"" + email + "\"," +
+                    "\"password\":\"" + password + "\"" +
+                "}"
+            )
+        );
+
+        HttpResponse<String> res;
+
+        try {
+            loginReq = HttpRequest.newBuilder()
+                .uri(loginUrl.toURI())
+                .POST(bp)
+                .headers("Content-Type", "application/json")
+                .build();
+            res = client.send(loginReq, BodyHandlers.ofString());
+            System.out.println(res.body());
+        } catch (Exception e) {
+            System.out.println(loginUrl);
+            System.out.println("error in login");
+            System.out.println(e);
+        }
+    }
+    */
+
+    // Uses the user token to get user info
+    public void getMe() {
+        System.out.println(meUrl);
+
+        HttpRequest req;
+        HttpResponse<String> res;
+
+        try {
+            req = HttpRequest.newBuilder()
+                .uri(meUrl.toURI())
+                .GET()
+                .headers("Authorization", token)
+                .build();
+            res = client.send(req, BodyHandlers.ofString());
+            me = new JSONObject(res.body().toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    // Uses a channel ID to get the last messages in a channel
+    // NOTE: The array is backwards! It goes from the last message to the first message
+    public JSONObject getMessages(String channelID, int limit) {
+        URL reqURL;
+
+        HttpRequest req;
+        HttpResponse<String> res;
+
+        String msgStrings = "";
+
+        try {
+            reqURL = new URL("https", "discordapp.com", "/api/v6/channels/" + channelID + "/messages?limit=" + limit);
+            req = HttpRequest.newBuilder()
+                .uri(reqURL.toURI())
+                .GET()
+                .headers("Authorization", token)
+                .build();
+            res = client.send(req, BodyHandlers.ofString());
+            msgStrings = "{\"messages\": " + res.body().toString() + "}";
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        JSONObject msgs = new JSONObject(msgStrings);
+        return msgs;
+    }
+
+    // WIP will be used when I implement DiscordWebSocket
+    private String getWebSocketGateway() {
+        HttpRequest req;
+        HttpResponse<String> res;
+
+        try {
+            req = HttpRequest.newBuilder()
+                .uri(gatewayUrl.toURI())
+                .GET()
+                .headers("Authorization", token)
+                .build();
+            res = client.send(req, BodyHandlers.ofString());
+            JSONObject resBody = new JSONObject(res.body().toString());
+            return resBody.getString("url");
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    // Sends a message to a channel, needs a channel ID
+    public void sendMessage(String channelId, String messageContent, boolean tts, String nonce) {
+        String body = new JSONObject()
+                    .put("content", messageContent)
+                    .put("tts", tts)
+                    .put("nonce", nonce)
+        .toString();
+
+        BodyPublisher bp = BodyPublishers.ofString(body);
+
+        HttpRequest req;
+        HttpResponse<String> res;
+        
+        try {
+            URL reqUrl = new URL("https", "discordapp.com", "/api/v6/channels/" + channelId + "/messages");
+            req = HttpRequest.newBuilder()
+                .uri(reqUrl.toURI())
+                .POST(bp)
+                .headers("Authorization", token, "Content-Type", "application/json")
+                .build();
+            res = client.send(req, BodyHandlers.ofString());
+            System.out.println(res.statusCode());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
